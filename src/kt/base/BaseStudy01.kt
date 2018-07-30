850f42bcda4c8716f47b774e35ce24d763b71ceb
lingxiaodshz <lingxiaodshz@163.com>
2018-07-30 22:11:02 +0800

    Kotlin变量、常量和类型推断

--- src/kt/base/BaseStudy01.kt
+++ src/kt/base/BaseStudy01.kt
@@ -9,7 +9,7 @@ fun main(args: Array<String>) {
     val a = 5
     val a1 = 1.5
     println(a === a)
-    print(a1)
+    println(a1)
 
 
 //    由于不同的表示方式，较小类型并不是较大类型的子类型，较小的类型不能隐式转换为较大的类型。
@@ -21,5 +21,21 @@ fun main(args: Array<String>) {
     val b: Byte = 1
     val i: Int = b.toInt()
 
+    //var用来声明普通变量，可以修改，val用来声明常亮，不能被修改
+    var c = 5
+    c = 6
+    println(c)
 
+    //Kotlin可以根据值自动推断数据类型，比如整数默认Int型，如果赋值Long型，会报错
+    var d = 555
+//    d = 555555555555
+    //可以显示的指定数据类型
+    var e: Long = 666
+    e = 6666666666666
+    println(e)
+
+    //如果定义变量不赋值同时不指定类型会报错的
+//    var g
+    //所以如果定义一个不赋值的变量，必须显示的指定类型
+    var g: Int
 }
