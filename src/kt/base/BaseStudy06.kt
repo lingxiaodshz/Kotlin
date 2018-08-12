d5209ea7b15165235f2d0fda7a15f8b04285fe4b
lingxiaodshz <lingxiaodshz@163.com>
2018-08-12 13:20:49 +0800

    Kotlin的枚举类型，以及object修饰代理类

--- src/kt/base/BaseStudy06.kt
+++ src/kt/base/BaseStudy06.kt
@@ -8,26 +8,35 @@ fun main(args: Array<String>) {
 
     var grandFather = GrandFather()
     grandFather.wash()
+
+    println(Season.Autumn.ordinal)
 }
 
 interface IWash {
     fun wash()
 }
 
-class BHSon : IWash {
+object BHSon : IWash {
     override fun wash() {
         println("儿子洗衣服")
     }
 }
 
 // 注意：此时SHFather类并没有重写wash方法，而是由BHSon代理执行
-class SHFather : IWash by BHSon() {
+class SHFather : IWash by BHSon {
 }
 
 // 代理之后仍然可以重写wash方法
-class GrandFather : IWash by BHSon() {
+class GrandFather : IWash by BHSon {
     override fun wash() {
         println("爷爷放好水")
-        BHSon().wash()
+        // 这种方式相当于又创建了一个BHSon()对象，如果想直接用的话，可已将BHSon的class改为object
+        // 这样BHSon在内存中就会只有一个对象，类似于静态
+        BHSon.wash()
     }
+}
+
+// 枚举
+enum class Season{
+    Spring,Summer,Autumn,Winter
 }
